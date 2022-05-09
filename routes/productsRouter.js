66825fe5bdea8f436b0fed9bb8c369385cf93327
TiLourinho <lourinho.tiago@gmail.com>
2022-05-09 11:11:21 -0300

    feat: products and sales validations

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,12 @@
 const productsRouter = require('express').Router();
 const rescue = require('express-rescue');
+const validateProducts = require('../middlewares/productsValidation');
 
 const ProductController = require('../controllers/ProductController');
 
 productsRouter.get('/products', rescue(ProductController.getAll));
 productsRouter.get('/products/:id', rescue(ProductController.getById));
+productsRouter.post('/products', validateProducts);
+productsRouter.put('/products/:id', validateProducts);
 
 module.exports = productsRouter;
--- routes/salesRouter.js
+++ routes/salesRouter.js
@@ -1,9 +1,12 @@
 const salesRouter = require('express').Router();
 const rescue = require('express-rescue');
+const validateSales = require('../middlewares/salesValidation');
 
 const SalesController = require('../controllers/SalesController');
 
 salesRouter.get('/sales', rescue(SalesController.getAll));
 salesRouter.get('/sales/:id', rescue(SalesController.getById));
+salesRouter.post('/sales', validateSales);
+salesRouter.put('/sales/:id', validateSales);
 
 module.exports = salesRouter;
